5cc22a60dbf1cae6a774d49255b5f7a161eae835
Cal Calcifer <calcalciffer@gmail.com>
2025-08-27 09:12:51 -0400

    mistake

--- src/main.py
+++ src/main.py
@@ -74,8 +74,7 @@ for bbg_ver in bbg_versions:
         for page in pages_list:
             page_name = page['name']
             get_page_function = page['func']
-            if page_name == 'changelog':
-                print(f'  page: {page_name}')
-                generate_html_file(bbg_ver, l, get_page_function, page_name)
+            print(f'  page: {page_name}')
+            generate_html_file(bbg_ver, l, get_page_function, page_name)
 
 generate_sitemap()
