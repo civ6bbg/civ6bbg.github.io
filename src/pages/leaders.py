300b271923a636264efc83d024691bd3d6209a32
Cal Calcifer <calcalciffer@gmail.com>
2025-09-12 23:19:05 -0400

    add policies page

--- src/pages/leaders.py
+++ src/pages/leaders.py
@@ -55,7 +55,7 @@ def get_leader_html_file(bbg_version, lang, pages_list):
                         unlock_tech = units_dict[item[3]][35]
                         unlock_civic = units_dict[item[3]][36]
                         tech_civic_dialog = get_unlock_tech_civic_dialog(unlock_tech, unlock_civic, locs_data, en_US_locs_data, tech_to_loc_dict, civic_to_loc_dict)
-                        base_game_tech_civic_dialog = ''
+                        base_game_tech_civic_dialog = None
                         if item[3] in base_game_units_dict:
                             unlock_tech = base_game_units_dict[item[3]][35]
                             unlock_civic = base_game_units_dict[item[3]][36]
@@ -63,7 +63,7 @@ def get_leader_html_file(bbg_version, lang, pages_list):
                                 unlock_tech, unlock_civic, locs_data, en_US_locs_data, tech_to_loc_dict, civic_to_loc_dict)
                         show_element_with_base_option(item[5], lang, locs_data, en_US_locs_data, 
                             data_append = (f'[NEWLINE][NEWLINE]{unlocked_by} {tech_civic_dialog}' if tech_civic_dialog != None else ''), 
-                            base_game_data_append = (f'[NEWLINE][NEWLINE]{unlocked_by} {base_game_tech_civic_dialog}' if tech_civic_dialog != None else ''))
+                            base_game_data_append = (f'[NEWLINE][NEWLINE]{unlocked_by} {base_game_tech_civic_dialog}' if base_game_tech_civic_dialog != None else ''))
                     else:
                         show_element_with_base_option(item[5], lang, locs_data, en_US_locs_data)
                     br()
